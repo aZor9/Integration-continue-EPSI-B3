0085618a192146edfc811c5ad0b92e04f2287995
Victor Bechet <victorbechet5@gmail.com>
2026-06-17 14:53:36 +0200

    Commit

--- src/test/java/com/epsi/tp/UserServiceTest.java
+++ src/test/java/com/epsi/tp/UserServiceTest.java
@@ -3,16 +3,16 @@ package com.epsi.tp;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class UserServiceTest {
+class UserServiceTest {
 
     @Test
-    private void testLoginAdmin() {
+    void testLoginAdmin() {
         UserService userService = new UserService();
         
         // On teste la méthode login avec les bons identifiants.
         // Ce test permet d'avoir une couverture de code partielle pour JaCoCo/SonarQube.
         assertDoesNotThrow(() -> {
-            userService.login("admin", "super_secret_password_123!");
+            userService.login(System.getenv("USER_USERNAME"), System.getenv("USER_PASSWORD"));
         });
     }
 }
