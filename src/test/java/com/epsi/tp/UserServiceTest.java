912890b9466a6b1a68e1d788988730456879b6d6
sisinou <sisinoudu34@gmail.com>
2026-06-18 20:00:47 +0200

    fix: use hardcoded username et psswrd

--- src/test/java/com/epsi/tp/UserServiceTest.java
+++ src/test/java/com/epsi/tp/UserServiceTest.java
@@ -12,7 +12,7 @@ class UserServiceTest {
         // On teste la méthode login avec les bons identifiants.
         // Ce test permet d'avoir une couverture de code partielle pour JaCoCo/SonarQube.
         assertDoesNotThrow(() -> {
-            userService.login(System.getenv("USER_USERNAME"), System.getenv("USER_PASSWORD"));
+            userService.login("admin", "admin123");
         });
     }
 
